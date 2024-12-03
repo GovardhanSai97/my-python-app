3cca7a3e79485153c120f32adbce97185b2ba6ce
Govardhan Sai <govardhansai1996panduri@gmail.com@gmail.com>
2024-12-02 19:28:31 -0600

    Added deployment scripts and appspec.yml

---
+++ scripts/before_install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+if [ -d /home/ubuntu/my-python-app ]; then
+    rm -rf /home/ubuntu/my-python-app
+fi
---
+++ scripts/start_server.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/ubuntu/my-python-app
+python3 -m pip install -r requirements.txt
+python3 app.py

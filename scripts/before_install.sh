936e36e78783fcb29472ab472b2813a725262abb
Govardhan Sai <govardhansai1996panduri@gmail.com@gmail.com>
2024-12-03 10:50:51 -0600

    updated appspecfile and there dependencies

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,10 @@
-#!/bin/bash
+if [ -d /home/ubuntu/my-python-app ]; then
+    rm -rf /home/ubuntu/my-python-app
+fi
+sudo apt-get update
+sudo apt-get install -y python3 python3-pip docker.io docker-compose
+sudo sytemctl start docker
+sudo systemctl enable docker#!/bin/bash
 if [ -d /home/ubuntu/my-python-app ]; then
     rm -rf /home/ubuntu/my-python-app
 fi
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 cd /home/ubuntu/my-python-app
-python3 -m pip install -r requirements.txt
-python3 app.py
+docker-compose up -d

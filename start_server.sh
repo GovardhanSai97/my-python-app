08497b81954be566657e1e27020b69e64c5471ae
Govardhan Sai <govardhansai1996panduri@gmail.com@gmail.com>
2024-12-04 00:38:29 -0600

    compose to run

--- start_server.sh
+++ start_server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd /home/ubuntu/my-python-app
 aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 577638362176.dkr.ecr.us-east-2.amazonaws.com
-docker compose up -d
+# docker compose up -d
+docker run 577638362176.dkr.ecr.us-east-2.amazonaws.com/my-python-app:latest
 

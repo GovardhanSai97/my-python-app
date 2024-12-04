97603c59490f7a27f14ba0e7f3e8dcb1de98062b
Govardhan Sai <govardhansai1996panduri@gmail.com@gmail.com>
2024-12-03 20:34:51 -0600

    all changes are made to work app

--- before_install.sh
+++ before_install.sh
@@ -2,7 +2,7 @@ if [ -d /home/ubuntu/my-python-app ]; then
     rm -rf /home/ubuntu/my-python-app
 fi
 sudo apt-get update
-sudo apt-get install -y python3 python3-pip docker.io docker-compose
+sudo apt-get install -y python3 python3-pip docker.io docker compose
 sudo sytemctl start docker
 aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 577638362176.dkr.ecr.us-east-2.amazonaws.com
 docker pull 577638362176.dkr.ecr.us-east-2.amazonaws.com/my-python-app:latest

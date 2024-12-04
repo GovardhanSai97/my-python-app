1b91d4484f92fa6746f8758ecc28f381be16a4fa
Govardhan Sai <govardhansai1996panduri@gmail.com@gmail.com>
2024-12-03 21:36:46 -0600

    yo

--- before_install.sh
+++ before_install.sh
@@ -4,6 +4,16 @@ fi
 sudo apt-get update
 sudo apt-get install -y python3 python3-pip docker.io docker-compose
 sudo sytemctl start docker
+if ! [ -x "$(command -v aws)" ]; then
+    echo "AWS CLI not installed. Installing now..."
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+    unzip awscliv2.zip
+    sudo ./aws/install
+    rm -rf awscliv2.zip aws/
+    echo "AWS CLI installed successfully."
+else
+    echo "AWS CLI is already installed."
+fi
 aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 577638362176.dkr.ecr.us-east-2.amazonaws.com
 docker pull 577638362176.dkr.ecr.us-east-2.amazonaws.com/my-python-app:latest
 sudo systemctl enable docker

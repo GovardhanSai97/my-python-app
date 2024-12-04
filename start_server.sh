9b727aa4849b1b363fb9803700b7b46a5daeaf36
Govardhan Sai <govardhansai1996panduri@gmail.com@gmail.com>
2024-12-03 20:52:00 -0600

    all changes are made to work app

--- start_server.sh
+++ start_server.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 cd /home/ubuntu/my-python-app
+aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 577638362176.dkr.ecr.us-east-2.amazonaws.com
 docker compose up -d
+
